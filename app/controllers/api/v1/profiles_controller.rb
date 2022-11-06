977ae62bf16e2921c583cfe99b9dbafbac534be7
thales-pinner <thales.neves@pinpeople.com.br>
2022-11-06 19:54:25 -0300

    Refactor controller, include active record to error messages

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -3,26 +3,41 @@ class Api::V1::ProfilesController < ApplicationController
   def create
     @profile = Profile.new(profile_params)
 
-    if @profile.save
-      api_response = Klaviyo.add_profile(@profile)
+    if @profile.valid?
+      response = Klaviyo.add_profile(@profile)
 
-      api_response.code == 204 ? render_success : render_error
+      api_result(response)
     else
-      render_error
+      message = @profile.errors.full_messages.join(', ')
+      render_error(message)
     end
   end
 
   private
 
+  def api_result(response)
+    return render_error(response) unless response.code == 204
+
+    @profile.save!
+    render_success
+  end
+
   def profile_params
     params.require(:profile).permit(:first_name, :last_name, :email, :phone_number, :birth_date)
   end
 
-  def render_error
-    render status: 401, json: { message: 'Invalid request.' }
+  def render_error(message)
+    render status: 401, json: { message: message }
   end
 
   def render_success
     render status: 200, json: { message: 'Success!' }
+  end
+
+  def api_error_message(api_response)
+    response = api_response['errors']
+    response.map do |res|
+      res['detail']
+    end
   end
 end
