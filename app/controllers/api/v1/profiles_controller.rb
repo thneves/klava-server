75be61ecc4d7eca50b66ca475ddc75de1d0a8f0f
thales-pinner <thales.neves@pinpeople.com.br>
2022-11-06 23:13:31 -0300

    fix typo

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -16,28 +16,29 @@ class Api::V1::ProfilesController < ApplicationController
   private
 
   def api_result(response)
-    return render_error(response) unless response.code == 204
+    return render_error(api_error_message(response)) unless response.code == 204
 
     @profile.save!
     render_success
-  end
-
-  def profile_params
-    params.require(:profile).permit(:first_name, :last_name, :email, :phone_number, :birth_date)
-  end
-
-  def render_error(message)
-    render status: 401, json: { message: message }
   end
 
   def render_success
     render status: 200, json: { message: 'Success!' }
   end
 
+  def render_error(message)
+    render status: 401, json: { message: message }
+  end
+
   def api_error_message(api_response)
     response = api_response['errors']
+    binding.pry
     response.map do |res|
       res['detail']
     end
   end
+end
+
+def profile_params
+  params.require(:profile).permit(:first_name, :last_name, :email, :phone_number, :birth_date)
 end
